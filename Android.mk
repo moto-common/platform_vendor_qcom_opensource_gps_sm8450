47d99e19ce3e1c73b44c7ec0db01cef1a470bff9
electimon <electimon@gmail.com>
2024-02-14 01:32:01 +0000

    adj

--- Android.mk
+++ Android.mk
@@ -3,7 +3,6 @@ ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 
 # Set required flags
 GNSS_CFLAGS := \
-    -Werror \
     -Wno-error=unused-parameter \
     -Wno-error=macro-redefined \
     -Wno-error=reorder \
